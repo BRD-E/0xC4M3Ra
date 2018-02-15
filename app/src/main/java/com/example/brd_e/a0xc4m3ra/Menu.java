94e42da8ea2159018e92afc7f0bebd9c65f278f1
Joshua Melton <BRD-E@outlook.com>
2018-02-14 19:22:11 -0700

    Got a button to open the camera

--- app/src/main/java/com/example/brd_e/a0xc4m3ra/Menu.java
+++ app/src/main/java/com/example/brd_e/a0xc4m3ra/Menu.java
@@ -1,7 +1,15 @@
 package com.example.brd_e.a0xc4m3ra;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +17,15 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button btn=findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+        });
     }
+    
 }
