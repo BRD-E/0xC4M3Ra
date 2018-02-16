6261ea286b9b21c32d4ed16a402c25251cd6afdb
Joshua Melton <BRD-E@outlook.com>
2018-02-15 18:38:47 -0700

    Got pixel data, modified pixel data to CYAN

--- app/src/main/java/com/example/brd_e/a0xc4m3ra/BackgroundTest.java
+++ app/src/main/java/com/example/brd_e/a0xc4m3ra/BackgroundTest.java
@@ -1,12 +1,14 @@
 package com.example.brd_e.a0xc4m3ra;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
         import android.content.Intent;
         import android.graphics.Bitmap;
         import android.os.Bundle;
-        import android.view.View;
+import android.util.Log;
+import android.view.View;
         import android.widget.Button;
         import android.widget.ImageView;
 
@@ -33,6 +35,27 @@ public class BackgroundTest extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
+            photo = photo.copy( Bitmap.Config.ARGB_8888 , true);
+            int width = photo.getWidth();
+            int height = photo.getHeight();
+            int color = photo.getPixel(0,0);
+            int a = Color.alpha(color);
+            int r = Color.red(color);
+            int g = Color.green(color);
+            int b = Color.blue(color);
+            for(int x = 0; x < width;x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    photo.setPixel(x,y, Color.CYAN);
+                }
+            }
+            color = photo.getPixel(0,0);
+            a = Color.alpha(color);
+            r = Color.red(color);
+            g = Color.green(color);
+            b = Color.blue(color);
+            Log.i("test","a: " + a + ",r: " + r + ",b: " + b + ",g: " + g);
             imageView.setImageBitmap(photo);
         }
     }
